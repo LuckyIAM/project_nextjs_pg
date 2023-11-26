5ef52c5efb7fe1a54e075968cc8b5bf49481637a
Andrei Statov <zavalisca.elena@gmail.com>
2023-11-26 22:33:25 +0300

    crud execute select * from news_news

---
+++ training_type_script/index.ts
@@ -0,0 +1,65 @@
+//nr.1
+// let test: string = 'is work'
+// alert(test)
+
+//nr.2
+// let test: string
+// test = 'is create a variable of type string'
+
+// let num: number
+// num = 123
+
+// let flag: boolean = true
+
+//nr. 3
+
+// test = 'change value'
+// console.log(test);
+
+
+
+// number = 'change value'
+// console.log(number);
+
+// nr. 4
+// let test1: number = 123;
+// let test2: number = 456;
+
+// console.log(test1 + test2);
+
+// let test1: string = '123';
+// let test2: string = '456';
+
+// let test3: number = test1 + test2;
+// console.log(test3);
+
+// let test1: number = 123;
+// let test2: number = 456;
+// let test3: string = '!';
+
+// let test4: string = test1 + test2 + test3;
+// console.log(test4);
+
+
+//nr. 5
+// let arr:string[] = ['a', 'b', 'c']
+// let arr2: Array<string> = ['a', 'b', 'c']
+
+
+// nr. 6
+// let date = {year: 2023, month: 11, day: 23}
+// console.log(date);
+
+//nr. 7
+// let date = {year: 2025, month: 12, day: 31};
+// date = {year: 2025, month: 122, date: 7};
+// let date = {year: 2025, month: 12, day: 31};
+// date = {year: 2025, month: 12};
+
+//nr.8
+let res: number = 0
+
+for(let i: number = 0; i < 100; i++){
+    res+=i
+}
+console.log(res);
